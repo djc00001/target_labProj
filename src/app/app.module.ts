4d5f2abc90e522695ca26aeaba4cee9965f2ac56
David Clark <djc00001@mix.wvu.edu>
2022-01-26 13:37:22 -0500

    Continued work on top nav bar begun work on second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TopNavBarComponent } from './header/topnavbar.component';
+import { SecondaryNavBarComponent } from './secondary header/secondarynavbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavBarComponent
+    TopNavBarComponent,
+    SecondaryNavBarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/secondary header/secondarynavbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "target-secondarynavbar",
+    templateUrl: "secondarynavbar.component.html",
+    styleUrls: ["secondarynavbar.component.css"]
+})
+export class SecondaryNavBarComponent{
+
+}
