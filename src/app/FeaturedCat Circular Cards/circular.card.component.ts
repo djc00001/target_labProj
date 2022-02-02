f15d33afb4eccff3ca278e033a237b1b078e109b
David Clark <djc00001@mix.wvu.edu>
2022-02-02 13:28:31 -0500

    Working on first card component

---
+++ src/app/FeaturedCat Circular Cards/circular.card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "target-circular-category-card",
+    templateUrl: "circular.card.component.html",
+    styleUrls: ["circular.card.component.css"]
+})
+export class CircularCard {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CircularCard } from './FeaturedCat Circular Cards/circular.card.component';
 import { TopLeftSideComponent } from './header/leftside.component';
 import { TopNavBarComponent } from './header/topnavbar.component';
 import { TopRightSideComponent } from './header/toprightside.component';
@@ -17,7 +18,8 @@ import { SecondaryNavBarComponent } from './secondary header/secondarynavbar.com
     TopRightSideComponent,
     SecondaryLeftSideComponent,
     SecondaryRightSideComponent,
-    SecondaryNavBarComponent
+    SecondaryNavBarComponent,
+    CircularCard
   ],
   imports: [
     BrowserModule
