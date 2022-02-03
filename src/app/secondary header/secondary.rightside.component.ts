38bdf17f01ec45b35777bf5f19adf363ebd873d6
David Clark <djc00001@mix.wvu.edu>
2022-02-02 22:40:15 -0500

    Added underline hover to ALL components

--- src/app/secondary header/secondary.rightside.component.ts
+++ src/app/secondary header/secondary.rightside.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "target-secondaryrightside",
-    templateUrl: "secondary.rightside.component.html"
+    templateUrl: "secondary.rightside.component.html",
+    styleUrls: ["secondary.rightside.component.css"]
 })
 export class SecondaryRightSideComponent {
 
