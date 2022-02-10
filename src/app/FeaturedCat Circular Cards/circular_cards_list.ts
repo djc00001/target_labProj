45cf3536398edb7bfa1979a9441950da7ed2f469
David Clark <djc00001@mix.wvu.edu>
2022-02-09 23:31:32 -0500

    Set up ability to use a list for other component

---
+++ src/app/FeaturedCat Circular Cards/circular_cards_list.ts
@@ -0,0 +1,26 @@
+export const circular_cards_list = [
+    {
+        img: "./assets/Circle-Comp/Valentines-Day-Target.png",
+        category: "Valentine's Day"
+    },
+    {
+        img: "./assets/Circle-Comp/Circle-Women.png",
+        category: "Women's"
+    },
+    {
+        img: "./assets/Circle-Comp/Circle-Men.png",
+        category: "Men's"
+    },
+    {
+        img: "./assets/Circle-Comp/Circle-Kids.png",
+        category: "Kids'"
+    },
+    {
+        img: "./assets/Circle-Comp/Circle-Baby.png",
+        category: "Baby"
+    },
+    {
+        img: "./assets/Circle-Comp/Circle-Home.png",
+        category: "Home"
+    }
+]
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { BoxcardCompModel } from './homepagead/boxcard_comp.model';
 import { mock_boxcard_list } from './homepagead/mock_boxcard_list';
+import { circular_cards_list } from './FeaturedCat Circular Cards/circular_cards_list';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,7 @@ import { mock_boxcard_list } from './homepagead/mock_boxcard_list';
 export class AppComponent {
   title = 'target-labProj';
   boxcards: BoxcardCompModel [] = [];
+  circular_cards_list: any = circular_cards_list;
 
   constructor() {
     for(var boxcard_comp of mock_boxcard_list){
