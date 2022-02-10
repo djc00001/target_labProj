832250485c7ed67516e5fa5ad5ef31555694b36c
David Clark <djc00001@mix.wvu.edu>
2022-02-09 22:51:09 -0500

    Finished Assignment 3 requirements for first comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BoxcardCompModel } from './homepagead/boxcard_comp.model';
+import { mock_boxcard_list } from './homepagead/mock_boxcard_list';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'target-labProj';
+  boxcards: BoxcardCompModel [] = [];
+
+  constructor() {
+    for(var boxcard_comp of mock_boxcard_list){
+      console.log(boxcard_comp);
+      this.boxcards.push(boxcard_comp)
+    }
+  }
 }
---
+++ src/app/homepagead/mock_boxcard_list.ts
@@ -0,0 +1,23 @@
+export const mock_boxcard_list = [
+    {
+        img: "./assets/Val-Ad/Buy2-Get1.png",
+        title: "Buy 2, get 1 free",
+        description: "games, puzzles, movies, books, video games & activity kits*"
+    },
+    {
+        img: "./assets/Val-Ad/Candy-Treats.png",
+        title: "Candy & Treats",
+        description: ""
+    },
+    {
+        img: "./assets/Val-Ad/Ways2Celebrate.png",
+        title: "4 ways to celebrate",
+        description: "Create new traditions with these Cupid-approved activities."
+    },
+    {
+        img: "./assets/Val-Ad/AllThings.png",
+        title: "All things Valentine's Day",
+        description: "Celebrate love with cards, decor & more."
+    }
+
+];
