e0cb5fcf1045469144e544d636e920a31f113360
David Clark <djc00001@mix.wvu.edu>
2022-02-24 02:39:06 -0500

    Begun ng loop for new component

---
+++ src/app/GroceryPage/grocerydeals_list.ts
@@ -0,0 +1,27 @@
+export const grocerydeals_list = [
+    {
+        image: './assets/Grocery/dietcoke.png',
+        itemName: 'Diet Coke - 12pk/12 fl oz Cans',
+        price: '$5.79'
+    },
+    {
+        image: './assets/Grocery/RegCoke.png',
+        itemName: 'Coca-Cola - 12pk/12 fl oz Cans',
+        price: '$5.79'
+    },
+    {
+        image: './assets/Grocery/Sprite.png',
+        itemName: 'Sprite - 12pk/12 fl oz Cans',
+        price: '$5.79'
+    },
+    {
+        image: './assets/Grocery/DrPepper.png',
+        itemName: 'Dr Pepper Soda - 12pk/12 fl oz Cans',
+        price: '$5.69'
+    },
+    {
+        image: './assets/Grocery/Lays.png',
+        itemName: 'Lay\'s Classic Potato Chips - 8oz',
+        price: '$3.59'
+    },
+]
